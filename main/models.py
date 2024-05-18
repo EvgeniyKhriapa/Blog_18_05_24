8e9d566e2329cee0560d7bb556e853d3c54a809b
EvgeniyKhriapa <house28101989@gmail.com>
2024-05-18 14:15:13 +0300

    first commit

---
+++ main/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """Модель теми"""
+    text = models.CharField(max_length=200, verbose_name='Тема')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+
+    class Meta:
+        verbose_name_plural = 'Теми'
+
+    def __str__(self):
+        """Повернення запису назви теми"""
+        return self.text
+
+
+class Entry(models.Model):
+    """Модель записів для теми"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Записи до тем'
+
+    def __str__(self):
+        """Повернення запису теми"""
+        return f"{self.text[:100]}..."
