a60918922edab5d62827b55634eb8301d2fec1ef
EvgeniyKhriapa <house28101989@gmail.com>
2024-05-30 14:45:32 +0300

    add reset password for email2

--- users/urls.py
+++ users/urls.py
@@ -15,7 +15,10 @@ urlpatterns = [
     path('profile_edit_user/', views.profile_edit_user, name='profile_edit_user'),
     # Сторінка зміни пароля
     path('password_change_user/', views.password_change_user, name='change_password_user'),
+    # Скидання пароля через електронну пошту
     path('password-reset/', password_reset_request, name='password_reset_request'),
+    # Відправлення запиту на зміну пароля
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
+    # Підтвердження про відправлення листа електронною поштою
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name='password_reset_complete'),
 ]
